7c85db2a72e65ea4ab5a7a6229c4c7ba9c5a50c8
unknown <fajardillah25@gmail.com>
2024-07-05 19:13:11 +0700

    Update

--- src/test/java/DateFormatTest.java
+++ src/test/java/DateFormatTest.java
@@ -1,2 +1,79 @@
-package PACKAGE_NAME;public class DateFormatTest {
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateFormatTest {
+
+
+    @Test
+    void testDateFormat() {
+        String pattern = "EEEE dd MMMM yyyy";//pattern Date yang ada di class SimpleDateFormat(sesuai java doc)
+        var simpleFormat = new SimpleDateFormat(pattern);
+
+        String format = simpleFormat.format(new Date());
+        System.out.println(format);
+
+        /*
+        jika kita tidak memasukan locale sendiri maka default locale akan sesuai dengan laptop yaitu bahasa inggris
+         */
+    }
+
+
+    @Test
+    void testDateFormatIndo() {
+        Locale indo = new Locale("id","ID");//jika kita ingin memaksa menggunakan locale sendiri
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE dd MMMM yyyy", indo);
+
+        String format = simpleDateFormat.format(new Date());
+        System.out.println(format);
+    }
+
+    @Test
+    void testDateFormatJapan() {
+        var pattern = "EEEE dd MMMM yyyy";
+        var japan = new Locale("ja","JP");
+
+        var dateJapan = new SimpleDateFormat(pattern,japan);
+
+        var format = dateJapan.format(new Date());
+        System.out.println(format);
+
+    }
+
+
+    @Test
+    void testDateParseJapan() {
+        var pattern = "EEEE dd MMMM yyyy";
+        var japan = new Locale("ja","JP");
+        var parseJapan = new SimpleDateFormat(pattern,japan);
+
+        try {
+           Date format = parseJapan.parse("金曜日 05 7月 2024");
+           System.out.println(format);
+        }catch (ParseException exception){
+            System.out.println("TERJADI ERROR : "+exception.getMessage());
+        }
+
+        /*
+        jika menggunakan parse(mengkonversi string ke date) kita harus menggunakan try catch
+         */
+
+    }
+
+    @Test
+    void testDateParseIndo() {
+        var pattern = "EEEE dd MMMM yyyy";
+        var indog = new Locale("id","ID");
+        var parseIndog = new SimpleDateFormat(pattern,indog);
+
+        try{
+            Date format = parseIndog.parse("Sabtu 30 Desember 2000");//Penulisan ini harus sesuai dengan format yang ada di pattern jika tidak akan exception
+            System.out.println(format);
+        }catch (ParseException exception){
+            System.out.println("TERJADI ERROR : "+exception.getMessage());
+        }
+    }
 }
--- src/test/java/NumberFormatTest.java
+++ src/test/java/NumberFormatTest.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class NumberFormatTest {
+import org.junit.jupiter.api.Test;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class NumberFormatTest {
+
+
+    @Test
+    void testNumberFormat() {
+
+        var numberFormat = NumberFormat.getInstance();
+        var format = numberFormat.format(10000000.255);
+        System.out.println(format);// hasil dari format ini akan sesuai dengan default Locale laptop
+    }
+
+    @Test
+    void testNumberFormatIndo() {
+        var indo = new Locale("id","ID");
+        NumberFormat numberFormat = NumberFormat.getInstance(indo);//jika kita ingin menggunakan locale sendiri kita bisa menambahkan parameter method getInstance dengan locale yang kita buat
+        var formatIndo = numberFormat.format(10000000.255);//akan menampilkan hasil sesuai dengan number format indo
+        System.out.println(formatIndo);
+    }
+
+    @Test
+    void testNumberParseIndo() {
+
+        var indo = new Locale("id","ID");
+        NumberFormat numberFormat = NumberFormat.getInstance(indo);
+
+        try {
+            var parseIndo = numberFormat.parse("10000000.255").doubleValue();
+            System.out.println(parseIndo);
+        }catch (ParseException exception){
+            System.out.println("Error : "+exception.getMessage());
+        }
+    }
 }
